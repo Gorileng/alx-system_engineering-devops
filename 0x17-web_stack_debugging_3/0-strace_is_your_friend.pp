abdbb498b3e6790d4d9fbf443d0e08df98e30104
Gorileng <keneilwe963@gmail.com>
2023-11-08 17:56:35 -0800

    updated

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes the bad `phpp` extensions to the `php` in a WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
